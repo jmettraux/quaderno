ba847b3bfe2249b8ae4647a9afaf6b8fdae9cab9
John Mettraux <jmettraux@gmail.com>
2010-09-10 22:54:11 +0900

    tabula rasa

---
+++ test.old/test.rb
@@ -0,0 +1,22 @@
+
+#
+# testing quaderno
+#
+# Mon May 31 09:45:27 JST 2010
+#
+
+files = Dir[File.join(File.dirname(__FILE__), 'qt_*.js')]
+
+if a = ARGV[0]
+  files = files.select { |f| f.match(a) }
+end
+
+`rhino -e '1'`
+js = ($?.exitstatus == 0) ? 'rhino -debug' : 'js'
+
+files.each do |t|
+  puts '. ' + t
+  o = `#{js} #{t} #{t}`
+  puts o if o.size > 0
+end
+
