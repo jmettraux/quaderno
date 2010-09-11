ab6c6d05dab80a8de5f83a71c72ee09a00700329
John Mettraux <jmettraux@gmail.com>
2010-09-11 22:33:47 +0900

    2nd successful render test

---
+++ test/qt_2_render_array_text.js
@@ -0,0 +1,41 @@
+
+//
+// testing quaderno
+//
+// Sat Sep 11 22:29:13 JST 2010
+//
+
+file = arguments[0];
+dir = file.split('/').slice(0, -1).join('/');
+load(dir + '/base.js');
+
+// 0
+
+var template = "  \n\
+text customers.     \n\
+"
+var data = { "customers": [ "alice", "bob", "charly" ] };
+
+Quaderno.render('quad', template, data, {});
+
+//print($('.quad_root')[0]);
+//print(JSON.stringify($('.quad_root')[0].toArray()));
+
+assertEqual(
+  ["div",{"class":"quad_root"},[
+    ["div",{"class":"quad_element"},[
+      ["input",{"class":"quad_id","type":"hidden","value":"customers"},[]],
+      ["div",{"class":"quad_element"},[
+        ["input",{"class":"quad_id","type":"hidden","value":".0"},[]],
+        ["input",{"class":"quad_type","type":"hidden","value":"text"},[]],
+        ["div",{"class":"quad_key quad_text"},["alice"]]]],
+      ["div",{"class":"quad_element"},[
+        ["input",{"class":"quad_id","type":"hidden","value":".1"},[]],
+        ["input",{"class":"quad_type","type":"hidden","value":"text"},[]],
+        ["div",{"class":"quad_key quad_text"},["bob"]]]],
+      ["div",{"class":"quad_element"},[
+        ["input",{"class":"quad_id","type":"hidden","value":".2"},[]],
+        ["input",{"class":"quad_type","type":"hidden","value":"text"},[]],
+        ["div",{"class":"quad_key quad_text"},["charly"]]]]]]]],
+  $('.quad_root')[0].toArray());
+
